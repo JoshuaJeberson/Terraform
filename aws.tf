bdc4cad26d74255874af68664278f0811333dda2
JoshuaJeberson <94227343+JoshuaJeberson@users.noreply.github.com>
2024-04-29 15:21:50 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,13 +1,10 @@
 provider "aws" {
   region     = "us-east-1"
-   access_key = "AKIA2UC3FFR3XWWIGIUR"
-   secret_key = "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"
 }
 
-resource "aws_instance" "myec2"{
+resource "aws_instance1" "my_one_ec2"{
     ami = "ami-00c39f71452c08778"
     instance_type = "t2.micro"
-    # tags = {
-    #     Name = "my-first-ec2"
-    # }
+    
+  }
 }
