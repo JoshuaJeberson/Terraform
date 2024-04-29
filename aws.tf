06a55faefd5d6c5a571d5ecbfda10c1255040070
JoshuaJeberson <94227343+JoshuaJeberson@users.noreply.github.com>
2024-04-29 15:29:18 +0530

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_instance1" "my_one_ec2"{
+resource "aws_instance" "my_one_ec2"{
     ami = "ami-00c39f71452c08778"
     instance_type = "t2.micro"
     
